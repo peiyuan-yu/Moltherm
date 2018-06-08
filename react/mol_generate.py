36c129702b66abe9789a25bd028e2ca4fad3b62d
Qi Wang <wqthu11@gmail.com>
2018-06-08 10:18:43 -0700

    add todo

--- react/mol_generate.py
+++ react/mol_generate.py
@@ -40,9 +40,9 @@ class ProductSplit:
         mol = self.mol
         reac1 = None
         reac2 = None
-        # TODO: current break_bond method in pymatgen use distances to separate reactants, due to lost bonding message in Molecule object -qw
-        # TODO: breaking circular mols can return same reac1 & reac2 but with different site sequence -qw
-        # TODO: if reac1 and reac2 are same reactants and setting wrong break_bond_sites can have problems -qw
+        # TODO: current break_bond method use distances to separate reactants, due to lost bonding message in Molecule object. -qw
+        # TODO: only supports break_chain_mol now. -qw
+        # TODO: output filename of react1 and react2 needs improvement as one molecule can be split many times. -qw
         for sites in break_bond_sites:
             reac1, reac2 = split_chain_mol(mol, sites)
             if MoleculeMatcher(reac1, reac2):
@@ -72,101 +72,3 @@ def split_chain_mol(molecule, break_bond_sites):
 
 # class ReactantJoin:
 
-
-
-
-
-# def mol_from_file(path, file):
-#     """
-#     Get pymatgen molecule object from input file.
-#     Args:
-#         path:
-#         file:
-#
-#     Returns:
-#         pymatgen molecule object
-#     """
-#     input_file = os.path.join(path, file)
-#     try:
-#         molecule = IMolecule.from_file(input_file)
-#         return molecule
-#     except AttributeError:
-#         print("This file {} cannot be parsed by pymatgen!".format(file))
-#
-#
-# def replace_number_of_bonds():
-#     with open(filename, 'r') as input_file, open('a.sdf', 'w') as output_file:
-#         for line in input_file:
-#             if line.strip() == '26 26  0  0  0  0            999 V2000':
-#                 output_file.write(' 26 24  0  0  0  0            999 V2000\n')
-#             else:
-#                 output_file.write(line)
-#
-# def replace_1st_bond():
-#     with open('a.sdf', 'r') as input_file, open('b.sdf', 'w') as output_file:
-#         for line in input_file:
-#             if line.strip() == '2  3  2  0  0  0  0':
-#                 output_file.write('  2  3  1  0  0  0  0\n')
-#             else:
-#                 output_file.write(line)
-#
-# def replace_2nd_bond():
-#     with open('b.sdf', 'r') as input_file, open('c.sdf', 'w') as output_file:
-#         for line in input_file:
-#             if line.strip() == '3  4  1  0  0  0  0':
-#                 output_file.write('  3  4  2  0  0  0  0\n')
-#             else:
-#                 output_file.write(line)
-#
-# def replace_3rd_bond():
-#     with open('c.sdf', 'r') as input_file, open('d.sdf', 'w') as output_file:
-#         for line in input_file:
-#             if line.strip() == '5  6  1  0  0  0  0':
-#                 output_file.write('  5  6  2  0  0  0  0\n')
-#             else:
-#                 output_file.write(line)
-#
-# def replace_4th_bond():
-#     with open('d.sdf', 'r') as input_file, open('e.sdf', 'w') as output_file:
-#         for line in input_file:
-#             if line.strip() == '1  2  1  0  0  0  0':
-#                 output_file.write('  1  2  2  0  0  0  0\n')
-#             else:
-#                 output_file.write(line)
-#
-# def delete_5th_bond():
-#     with open('e.sdf', 'r') as input_file, open('f.sdf', 'w') as output_file:
-#         for line in input_file:
-#             if line.strip() == '4  5  1  0  0  0  0':
-#                 output_file.write('')
-#             else:
-#                 output_file.write(line)
-#
-# def delete_6th_bond():
-#     with open('f.sdf', 'r') as input_file, open(filename, 'w') as output_file:
-#         for line in input_file:
-#             if line.strip() == '1  6  1  0  0  0  0':
-#                 output_file.write('')
-#             else:
-#                 output_file.write(line)
-#
-# replace_number_of_bonds()
-#
-# replace_1st_bond()
-#
-# replace_2nd_bond()
-#
-# replace_3rd_bond()
-#
-# replace_4th_bond()
-#
-# delete_5th_bond()
-#
-# delete_6th_bond()
-#
-# os.remove('a.sdf')
-# os.remove('b.sdf')
-# os.remove('c.sdf')
-# os.remove('d.sdf')
-# os.remove('e.sdf')
-# os.remove('f.sdf')
