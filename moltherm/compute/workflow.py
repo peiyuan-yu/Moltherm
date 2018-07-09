420de335c87cd7484688285f6559a24fb21c270f
Evan Spotte-Smith <EWCSpotteSmith@lbl.gov>
2018-07-09 16:39:30 -0700

    Enthalpy changes should be negative

--- moltherm/compute/workflow.py
+++ moltherm/compute/workflow.py
@@ -613,7 +613,7 @@ class MolThermAnalysis:
         # Generate totals as ∆H = H_pro - H_rct, ∆S = S_pro - S_rct
         # Also ensures that units are appropriate (Joules/mol,
         # rather than cal/mol or kcal/mol)
-        thermo_data["enthalpy"] = (pro_thermo["enthalpy"] - rct_thermo["enthalpy"]) * 1000 * 4.184
+        thermo_data["enthalpy"] = (pro_thermo["enthalpy"] - rct_thermo["enthalpy"]) * 1000 * 4.184 * -1
         thermo_data["entropy"] = (pro_thermo["entropy"] - rct_thermo["entropy"]) * 4.184
         try:
             thermo_data["t_critical"] = thermo_data["enthalpy"] / thermo_data["entropy"]
