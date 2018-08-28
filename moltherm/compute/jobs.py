623085800c9d9c2174a056b59a47d0bb0538b162
Evan Spotte-Smith <espottesmith@gmail.com>
2018-08-28 08:39:28 -0400

    fixed error in backup from new Custodian changes

--- moltherm/compute/jobs.py
+++ moltherm/compute/jobs.py
@@ -44,7 +44,8 @@ class QCJob(Job):
                  suffix="",
                  scratch_dir="/dev/shm/qcscratch/",
                  save_scratch=False,
-                 save_name="default_save_name"):
+                 save_name="default_save_name",
+                 backup=True):
         """
         Args:
             qchem_command (str): Command to run QChem.
@@ -62,6 +63,8 @@ class QCJob(Job):
                 Defaults to False.
             save_name (str): Name of the saved scratch directory. Defaults to
                 to "default_save_name".
+            backup (bool): Whether to backup the initial input file. If True, the
+                input will be copied with a ".orig" appended. Defaults to True.
         """
         self.qchem_command = qchem_command.split(" ")
         self.multimode = multimode
@@ -73,6 +76,7 @@ class QCJob(Job):
         self.scratch_dir = scratch_dir
         self.save_scratch = save_scratch
         self.save_name = save_name
+        self.backup = backup
 
     @property
     def current_command(self):
@@ -98,6 +102,8 @@ class QCJob(Job):
         return command
 
     def setup(self):
+        if self.backup:
+            shutil.copy(self.input_file, "{}.orig".format(self.input_file))
         os.putenv("QCSCRATCH", self.scratch_dir)
         if self.multimode == 'openmp':
             os.putenv('QCTHREADS', str(self.max_cores))
