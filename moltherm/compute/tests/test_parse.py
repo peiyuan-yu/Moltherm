0e48a9a6062d1d8d9d12c15558151289a99439ae
Evan Spotte-Smith <espottesmith@gmail.com>
2019-01-04 15:07:02 -0500

    Changed (and added) test files, continued slowly working on tests for parsers

--- moltherm/compute/tests/test_parse.py
+++ moltherm/compute/tests/test_parse.py
@@ -3,6 +3,10 @@ from os.path import join, dirname, abspath
 import shutil
 import unittest
 
+import bs4
+
+from pymatgen.io.babel import BabelMolAdaptor
+
 from moltherm.compute.parse import ReaxysParser, EPISuiteParser
 
 
@@ -18,13 +22,54 @@ files_dir = join(module_dir, "..", "..", "..", "test_files")
 
 class TestReaxysParser(unittest.TestCase):
     def setUp(self):
-        pass
+        self.filename = "reaxys.xml"
+        self.parser = ReaxysParser(files_dir)
 
     def tearDown(self):
-        pass
+        del self.parser
+        del self.filename
 
     def test_parse_reaxys_xml(self):
-        pass
+
+        parsed = self.parser.parse_reaxys_xml(self.filename)
+
+        # Ensure that all proper reactions are present
+        self.assertEqual(len(parsed), 3)
+
+        indices = [x["meta"]["index"] for x in parsed]
+        for index in [1, 9, 10]:
+            self.assertTrue(index in indices)
+
+        rxn_ids = [x["meta"]["rxn_id"] for x in parsed]
+        for rxn_id in [28100547, 3553459, 8633298]:
+            self.assertTrue(rxn_id in rxn_ids)
+
+        # Ensure that all reactions are actually appropriate
+        for rxn in parsed:
+            pro_species = list()
+            rct_species = list()
+
+            for pro in rxn["pros"]:
+                molecule = BabelMolAdaptor.from_string(pro, "mol")
+                molecule.add_hydrogen()
+                mol = molecule.pymatgen_mol
+                pro_species += [str(site.specie) for site in mol]
+            for rct in rxn["rcts"]:
+                molecule = BabelMolAdaptor.from_string(rct, "mol")
+                molecule.add_hydrogen()
+                mol = molecule.pymatgen_mol
+                rct_species += [str(site.specie) for site in mol]
+
+            self.assertSequenceEqual(sorted(pro_species), sorted(rct_species))
+
+        # Test one reaction to make sure metadata is recorded correctly
+        rxn = parsed[-1]["meta"]
+        self.assertEqual(rxn["solvents"], {'dichloromethane'})
+        self.assertEqual(len(rxn["pro_meta"]), 1)
+        self.assertEqual(len(rxn["rct_meta"]), 2)
+        self.assertSequenceEqual(rxn["pro_meta"], [(5424566, '(4,5-dimethylcyclohexa-1,4-dienyl)trimethylsilane')])
+        self.assertSequenceEqual(rxn["rct_meta"], [(605285, '2,3-dimethyl-buta-1,3-diene'),
+                                                   (906752, 'trimethylsilylacetylene')])
 
     def test_get_unique_reactions(self):
         pass
