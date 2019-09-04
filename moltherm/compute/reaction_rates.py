89ec6395f623bfdfad1c97f4bd0c3eb30c353c10
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2019-09-04 10:39:12 -0400

    Added basic thermodynamics to ReactionRateCalculator

--- moltherm/compute/reaction_rates.py
+++ moltherm/compute/reaction_rates.py
@@ -166,3 +166,169 @@ class ReactionRateCalculator:
         pro_comps = [p.composition for p in pro_mols]
 
         return Reaction(rct_comps, pro_comps)
+
+    @property
+    def net_energy(self):
+        """
+        Calculate net reaction energy.
+        """
+        rct_energies = [r["energy"] for r in self.reactants]
+        pro_energies = [p["energy"] for p in self.products]
+
+        return sum(pro_energies) - sum(rct_energies)
+
+    @property
+    def net_enthalpy(self):
+        """
+        Calculate net reaction enthalpy.
+        """
+        rct_enthalpies = [r["enthalpy"] for r in self.reactants]
+        pro_enthalpies = [p["enthalpy"] for p in self.products]
+
+        return sum(pro_enthalpies) - sum(rct_enthalpies)
+
+    @property
+    def net_entropy(self):
+        """
+        Calculate net reaction entropy.
+        """
+        rct_entropies = [r["entropy"] for r in self.reactants]
+        pro_entropies = [p["entropy"] for p in self.products]
+
+        return sum(pro_entropies) - sum(rct_entropies)
+
+    def calculate_net_gibbs(self, temperature):
+        """
+        Calculate net reaction Gibbs free energy at a given temperature.
+
+        ΔG = ΔH - T ΔS
+
+        Args:
+            temperature (float): absolute temperature in Kelvin
+
+        Returns:
+            float: net Gibbs free energy
+        """
+
+        return self.net_enthalpy - temperature * self.net_entropy
+
+    def calculate_net_thermo(self, temperature=300.0):
+        """
+        Calculate net energy, enthalpy, and entropy for the reaction.
+
+        Args:
+            temperature (float): absolute temperature in Kelvin (default 300.0K)
+
+        Returns:
+            net_thermo: dict with relevant net thermodynamic variables
+        """
+
+        net_thermo = {"energy": self.net_energy,
+                      "enthalpy": self.net_enthalpy,
+                      "entropy": self.net_entropy,
+                      "gibbs": self.calculate_net_gibbs(temperature)}
+
+        return net_thermo
+
+    def calculate_act_energy(self, reverse=False):
+        """
+        Calculate energy of activation.
+
+        Args:
+            reverse (bool): if True (default False), consider the reverse reaction; otherwise,
+                consider the forwards reaction
+
+        Returns:
+            float: energy of activation
+
+        """
+
+        trans_energy = self.transition_state["energy"]
+
+        if reverse:
+            pro_energies = [p["energy"] for p in self.products]
+            return trans_energy - sum(pro_energies)
+        else:
+            rct_energies = [r["energy"] for r in self.reactants]
+            return trans_energy - sum(rct_energies)
+
+    def calculate_act_enthalpy(self, reverse=False):
+        """
+        Calculate enthalpy of activation.
+
+        Args:
+            reverse (bool): if True (default False), consider the reverse reaction; otherwise,
+                consider the forwards reaction
+
+        Returns:
+            float: enthalpy of activation
+
+        """
+
+        trans_enthalpy = self.transition_state["enthalpy"]
+
+        if reverse:
+            pro_enthalpies = [p["enthalpy"] for p in self.products]
+            return trans_enthalpy - sum(pro_enthalpies)
+        else:
+            rct_enthalpies = [r["enthalpy"] for r in self.reactants]
+            return trans_enthalpy - sum(rct_enthalpies)
+
+    def calculate_act_entropy(self, reverse=False):
+        """
+        Calculate entropy of activation.
+
+        Args:
+            reverse (bool): if True (default False), consider the reverse reaction; otherwise,
+                consider the forwards reaction
+
+        Returns:
+            float: entropy of activation
+
+        """
+
+        trans_entropy = self.transition_state["entropy"]
+
+        if reverse:
+            pro_entropies = [p["entropy"] for p in self.products]
+            return trans_entropy - sum(pro_entropies)
+        else:
+            rct_entropies = [r["entropy"] for r in self.reactants]
+            return trans_entropy - sum(rct_entropies)
+
+    def calculate_act_gibbs(self, temperature, reverse=False):
+        """
+        Calculate Gibbs free energy of activation at a given temperature.
+
+        ΔG = ΔH - T ΔS
+
+        Args:
+            temperature (float): absolute temperature in Kelvin
+            reverse (bool): if True (default False), consider the reverse reaction; otherwise,
+                consider the forwards reaction
+
+        Returns:
+            float: Gibbs free energy of activation
+        """
+
+        return self.calculate_act_enthalpy() - temperature * self.calculate_act_entropy()
+
+    def calculate_activation_thermo(self, temperature=300.0, reverse=False):
+        """
+        Calculate thermodynamics of activation for the reaction.
+
+        Args:
+            temperature (float): absolute temperature in Kelvin (default 300.0K)
+            reverse (bool): if True (default False), consider the reverse reaction; otherwise,
+                consider the forwards reaction
+
+        Returns:
+            act_thermo: dict with relevant activation thermodynamic variables
+        """
+
+        net_thermo = {"energy": self.calculate_act_energy(),
+                      "enthalpy": self.calculate_act_enthalpy(),
+                      "entropy": self.calculate_act_entropy(),
+                      "gibbs": self.calculate_act_gibbs(temperature)}
+
+        return net_thermo
