accd1a8a91127841c8b8ad5a03a9a01c11fb6edc
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2019-09-04 16:19:43 -0400

    Changes to use MoleculeEntry rather than just atomate task_doc dicts

--- moltherm/compute/reaction_rates.py
+++ moltherm/compute/reaction_rates.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.constants import h, k, R
 
 from pymatgen.core.structure import Molecule
+from pymatgen.analysis.graphs import MoleculeGraph
+from pymatgen.entries.mol_entry import MoleculeEntry
 from pymatgen.analysis.reaction_calculator import Reaction, ReactionError
 
 from atomate.qchem.database import QChemCalcDb
@@ -23,29 +25,26 @@ class ReactionRateCalculator:
     An object which represents a chemical reaction (in terms of reactants, transition state,
     and products) and which can, from the energetics of those individual molecules, predict the
     rate constant, rate law, and thus the chemical kinetics of the reaction.
+
+    NOTE: It is assumed that only one transition state is present.
+
+    Args:
+        reactants (list): list of MoleculeEntry objects
+        products (list): list of MoleculeEntry objects
+        transition_state (MoleculeEntry): MoleculeEntry representing the transition state between
+            the reactants and the products
+        reaction (dict, or None): optional. If None (default), the "reactants" and
+        "products" lists will serve as the basis for a Reaction object which represents the
+        balanced stoichiometric reaction. Otherwise, this dict will show the number of molecules
+        present in the reaction for each reactant and each product in the reaction.
+
+    Returns:
+        None
     """
 
     def __init__(self, reactants, products, transition_state, reaction=None):
         """
-        If constructing this class manually, the use of one of the constructor methods is strongly
-        preferred.
 
-        NOTE: It is assumed that only one transition state is present.
-
-        Args:
-            reactants (list): list of dicts with reactant thermodynamics (energy, enthalpy, entropy)
-                and other parameters (including Molecule object representing structure)
-            products (list): list of dicts with product thermodynamics (energy, enthalpy, entropy)
-                and other parameters (including Molecule object representing structure)
-            transition_state (dict): dict with transition state thermodynamics (energy, enthalpy,
-                entropy) and other parameters (including Molecule object representing structure)
-            reaction (dict, or None): optional. If None (default), the "reactants" and
-            "products" lists will serve as the basis for a Reaction object which represents the
-            balanced stoichiometric reaction. Otherwise, this dict will show the number of molecules
-            present in the reaction for each reactant and each product in the reaction.
-
-        Returns:
-            None
         """
 
         self.reactants = reactants
@@ -53,8 +52,8 @@ class ReactionRateCalculator:
         self.transition_state = transition_state
 
         if reaction is None:
-            rct_mols = [r["molecule"] for r in self.reactants]
-            pro_mols = [p["molecule"] for p in self.reactions]
+            rct_mols = [r.mol_graph.molecule for r in self.reactants]
+            pro_mols = [p.mol_graph.molecule for p in self.reactions]
             try:
                 self.reaction = self.generate_reaction(rct_mols, pro_mols)
             except ReactionError:
@@ -78,7 +77,7 @@ class ReactionRateCalculator:
             transition_state (dict): dict representing task doc for transition state molecule
 
         Returns:
-            None
+            ReactionRateCalculator
         """
 
         rcts = list()
@@ -86,63 +85,44 @@ class ReactionRateCalculator:
 
         # Construct main dicts with molecular properties
         for rct_doc in reactants:
-            reactant = dict()
             try:
-                reactant["id"] = rct_doc["task_id"]
-                reactant["label"] = rct_doc["task_label"]
-                reactant["smiles"] = rct_doc["smiles"]
-                # convert all energies from Hartree to kcal/mol
-                reactant["energy"] = rct_doc["output"]["final_energy"] * 627.509474
-                reactant["enthalpy"] = rct_doc["output"]["enthalpy"]
-                reactant["entropy"] = rct_doc["output"]["entropy"]
-                reactant["molecule"] = Molecule.from_dict(rct_doc["output"]["optimized_molecule"])
-
-                rcts.append(reactant)
-
+                rcts.append(MoleculeEntry(Molecule.from_dict(rct_doc["output"]["optimized_molecule"]),
+                                          rct_doc["output"]["final_energy"],
+                                          enthalpy=rct_doc["output"]["enthalpy"],
+                                          entropy=rct_doc["output"]["entropy"],
+                                          entry_id=rct_doc["task_id"]))
             except KeyError:
                 raise ValueError("Reactant task doc does not follow schema! Docs must contain"
                                  " an output field with subfields optimized_geometry, final_energy,"
                                  " enthalpy, and entropy.")
 
         for pro_doc in products:
-            product = dict()
-
             try:
-                product["id"] = pro_doc["task_id"]
-                product["label"] = pro_doc["task_label"]
-                product["smiles"] = pro_doc["smiles"]
-                # convert all energies from Hartree to kcal/mol
-                product["energy"] = pro_doc["output"]["final_energy"] * 627.509474
-                product["enthalpy"] = pro_doc["output"]["enthalpy"]
-                product["entropy"] = pro_doc["output"]["entropy"]
-                product["molecule"] = Molecule.from_dict(pro_doc["output"]["optimized_molecule"])
-
-                pros.append(product)
+                pros.append(MoleculeEntry(Molecule.from_dict(pro_doc["output"]["optimized_molecule"]),
+                                          pro_doc["output"]["final_energy"],
+                                          enthalpy=pro_doc["output"]["enthalpy"],
+                                          entropy=pro_doc["output"]["entropy"],
+                                          entry_id=pro_doc["task_id"]))
 
             except KeyError:
                 raise ValueError("Product task doc does not follow schema! Docs must contain"
                                  " an output field with subfields optimized_geometry, final_energy,"
                                  " enthalpy, and entropy.")
 
-        transition = dict()
         try:
-            transition["id"] = transition_state["task_id"]
-            transition["label"] = transition_state["task_label"]
-            transition["smiles"] = transition_state["smiles"]
-            # convert all energies from Hartree to kcal/mol
-            transition["energy"] = transition_state["output"]["final_energy"] * 627.509474
-            transition["enthalpy"] = transition_state["output"]["enthalpy"]
-            transition["entropy"] = transition_state["output"]["entropy"]
-            transition["molecule"] = Molecule.from_dict(transition_state["output"]["optimized_molecule"])
-
+            transition = MoleculeEntry(Molecule.from_dict(transition_state["output"]["optimized_molecule"]),
+                                       transition_state["output"]["final_energy"],
+                                       enthalpy=transition_state["output"]["enthalpy"],
+                                       entropy=transition_state["output"]["entropy"],
+                                       entry_id=transition_state["task_id"])
         except KeyError:
             raise ValueError("Transition state task doc does not follow schema! Docs must contain"
                              " an output field with subfields optimized_geometry, final_energy,"
                              " enthalpy, and entropy.")
 
         # Calculate stoichiometry
-        rct_mols = [r["molecule"] for r in rcts]
-        pro_mols = [p["molecule"] for p in pros]
+        rct_mols = [r.mol_graph.molecule for r in rcts]
+        pro_mols = [p.mol_graph.molecule for p in pros]
         reaction = cls.generate_reaction(rct_mols, pro_mols)
 
         return cls(rcts, pros, transition, reaction=reaction)
@@ -173,8 +153,8 @@ class ReactionRateCalculator:
         """
         Calculate net reaction energy.
         """
-        rct_energies = [r["energy"] for r in self.reactants]
-        pro_energies = [p["energy"] for p in self.products]
+        rct_energies = [r.energy for r in self.reactants]
+        pro_energies = [p.energy for p in self.products]
 
         return sum(pro_energies) - sum(rct_energies)
 
@@ -183,8 +163,8 @@ class ReactionRateCalculator:
         """
         Calculate net reaction enthalpy.
         """
-        rct_enthalpies = [r["enthalpy"] for r in self.reactants]
-        pro_enthalpies = [p["enthalpy"] for p in self.products]
+        rct_enthalpies = [r.enthalpy for r in self.reactants]
+        pro_enthalpies = [p.enthalpy for p in self.products]
 
         return sum(pro_enthalpies) - sum(rct_enthalpies)
 
@@ -193,8 +173,8 @@ class ReactionRateCalculator:
         """
         Calculate net reaction entropy.
         """
-        rct_entropies = [r["entropy"] for r in self.reactants]
-        pro_entropies = [p["entropy"] for p in self.products]
+        rct_entropies = [r.entropy for r in self.reactants]
+        pro_entropies = [p.entropy for p in self.products]
 
         return sum(pro_entropies) - sum(rct_entropies)
 
@@ -244,13 +224,13 @@ class ReactionRateCalculator:
 
         """
 
-        trans_energy = self.transition_state["energy"]
+        trans_energy = self.transition_state.energy
 
         if reverse:
-            pro_energies = [p["energy"] for p in self.products]
+            pro_energies = [p.energy for p in self.products]
             return trans_energy - sum(pro_energies)
         else:
-            rct_energies = [r["energy"] for r in self.reactants]
+            rct_energies = [r.energy for r in self.reactants]
             return trans_energy - sum(rct_energies)
 
     def calculate_act_enthalpy(self, reverse=False):
@@ -266,13 +246,13 @@ class ReactionRateCalculator:
 
         """
 
-        trans_enthalpy = self.transition_state["enthalpy"]
+        trans_enthalpy = self.transition_state.enthalpy
 
         if reverse:
-            pro_enthalpies = [p["enthalpy"] for p in self.products]
+            pro_enthalpies = [p.enthalpy for p in self.products]
             return trans_enthalpy - sum(pro_enthalpies)
         else:
-            rct_enthalpies = [r["enthalpy"] for r in self.reactants]
+            rct_enthalpies = [r.enthalpy for r in self.reactants]
             return trans_enthalpy - sum(rct_enthalpies)
 
     def calculate_act_entropy(self, reverse=False):
@@ -288,13 +268,13 @@ class ReactionRateCalculator:
 
         """
 
-        trans_entropy = self.transition_state["entropy"]
+        trans_entropy = self.transition_state.entropy
 
         if reverse:
-            pro_entropies = [p["entropy"] for p in self.products]
+            pro_entropies = [p.entropy for p in self.products]
             return trans_entropy - sum(pro_entropies)
         else:
-            rct_entropies = [r["entropy"] for r in self.reactants]
+            rct_entropies = [r.entropy for r in self.reactants]
             return trans_entropy - sum(rct_entropies)
 
     def calculate_act_gibbs(self, temperature, reverse=False):
@@ -377,11 +357,11 @@ class ReactionRateCalculator:
                                                      kappa=kappa)
 
         if reverse:
-            pro_comps = [p["molecule"].composition for p in self.products]
+            pro_comps = [p.mol_graph.molecule.composition for p in self.products]
             exponents = np.array([self.reaction.get_coeff(comp) for comp in pro_comps])
             rate = rate_constant * np.array(concentrations) ** exponents
         else:
-            rct_comps = [r["molecule"].composition for r in self.reactants]
+            rct_comps = [r.mol_graph.molecule.composition for r in self.reactants]
             exponents = np.array([self.reaction.get_coeff(comp) for comp in rct_comps])
             rate = rate_constant * np.array(concentrations) ** exponents
 
